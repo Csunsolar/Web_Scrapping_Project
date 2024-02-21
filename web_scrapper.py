78e6b7cf066ca77cae1b82821bce7314ec0db019
Csunsolar <christophersun09@gmail.com>
2024-02-20 22:25:34 -0600

    cookie clicker commit

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,28 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from  selenium.webdriver.common.by import By
 import time
-
-Link = "https://quotes.toscrape.com/"
+from pynput import keyboard
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
+Link = "https://orteil.dashnet.org/cookieclicker/"
 driver.get(Link)
 
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="langSelect-EN"]'))
+)
 
+language_element = driver.find_element(By.XPATH, '//*[@id="langSelect-EN"]')
+language_element.click()
 
-try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "col-md-4"))
-    )
-finally:
-    input_element = driver.find_element(By.CLASS_NAME, "col-md-4").click() #this is for input
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.ID, 'bigCookie'))
+)
 
+cookie_element = driver.find_element(By.ID, 'bigCookie')
+cookie_element.click()
 
-time.sleep(10)
+time.sleep(30)
 
 driver.quit()
